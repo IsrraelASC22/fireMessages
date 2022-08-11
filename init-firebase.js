af17fac99e688ebac5af31c3f65017c97727f321
unknown <gitvvvgit config --global user.email 13odyssey13@gmail.com>
2022-08-11 07:32:32 -0700

    msgthing

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyCK2v4QqkgkKu5as99dPzcnKJGTYOVh1qM",
+            authDomain: "firemessage-212cb.firebaseapp.com",
+            projectId: "firemessage-212cb",
+            storageBucket: "firemessage-212cb.appspot.com",
+            messagingSenderId: "378888128517",
+            appId: "1:378888128517:web:9118ebc166664232f5746c",
+            measurementId: "G-GGX1XGXJPH"
+          };
+        
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
